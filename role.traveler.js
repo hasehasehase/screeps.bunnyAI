461d7320f3b01dd41dec36456c1674c0a30b585e
hasehasehase <max.haselmayer@gmail.com>
2017-05-18 18:32:28 +0200

    do minor changes, comments

--- role.traveler.js
+++ role.traveler.js
@@ -5,16 +5,20 @@ var roleTraveler = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy == 0 && creep.memory.working == true){
+      // Reset creep
+        if(creep.carry.energy == 0 && creep.memory.working){
             creep.memory.working = false;
             creep.memory.waypoint1 = false;
         }
-        if(creep.carry.energy == creep.carryCapacity && creep.memory.working == false){
+      //Go to work if full
+        if(creep.carry.energy == creep.carryCapacity && !creep.memory.working){
             creep.memory.working = true;
         }
-        if(creep.memory.working == false && creep.memory.waypoint1 == false){
+      //Go to source waypoint
+        if(!creep.memory.working && !creep.memory.waypoint1){
           creep.moveTo(waypoint);
-            if(creep.pos == '[room E93N34 pos 6,16]' && creep.memory.waypoint1 == false){
+          //At waypoint, decide which source to take
+            if(creep.pos == '[room E93N34 pos 6,16]' && !creep.memory.waypoint1){
                 creep.memory.waypoint1 = true;
                 var rdm = Math.floor(Math.random() * (100) + 1)
                 if(rdm>=50){
@@ -26,13 +30,14 @@ var roleTraveler = {
 
             }
         }
+        // FARM
         else if(creep.memory.waypoint1 && creep.carry.energy < creep.carryCapacity && creep.room !='[room E92N34]'){
             var sources = creep.room.find(FIND_SOURCES)
             if(creep.harvest(sources[creep.memory.source]) == ERR_NOT_IN_RANGE) {
                creep.moveTo(sources[creep.memory.source]);
             }
         }
-
+        //WORK
         else if(creep.memory.working){
             if(creep.room != '[room E92N34]'){
                 creep.moveTo(new RoomPosition(48,35,'E92N34'))
--- role.upgrader.js
+++ role.upgrader.js
@@ -1,32 +1,32 @@
 var roleUpgrader = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
-
-        if(creep.memory.upgrading && creep.carry.energy == 0) {
+    ru77n: function(creep) {
+      // Reset creep
+      if(creep.memory.upgrading && creep.carry.energy == 0) {
             creep.memory.upgrading = false;
-            creep.say('🔄 harvest');
 	    }
+      // Send creep to work
 	    if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
 	        creep.memory.upgrading = true;
-	        creep.say('⚡ upgrade');
 	    }
-
+      //Work
 	    if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-//                creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
-                creep.moveTo(creep.room.controller);
-            }
+        // Solution for Room E92N34
+        if(creep.pos.x == 36){
+            creep.moveTo(creep.room.controller)
         }
-        else {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
-            if(creep.pos.x == 36 && creep.memory.upgrading == true){
-                creep.moveTo(creep.room.controller)
-            }
+        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(creep.room.controller);
         }
+      }
+      //harvest
+      else {
+        var sources = creep.room.find(FIND_SOURCES);
+        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0]);
+        }
+      }
 	}
 };
 
